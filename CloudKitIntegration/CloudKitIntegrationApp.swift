793e19fc2c904c714797c8439c594d296293f89c
elianrichard <elianrichards@gmail.com>
2024-05-30 14:00:40 +0700

    Initial Commit

---
+++ CloudKitIntegration/CloudKitIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitIntegrationApp.swift
+//  CloudKitIntegration
+//
+//  Created by Elian Richard on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
